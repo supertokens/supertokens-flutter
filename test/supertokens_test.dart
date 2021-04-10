0ba9c3f87f18b16a7d18ddb94a9c2a6b357fda1c
Nemi Shah <nemishah1212@gmail.com>
2021-04-10 18:03:43 +0530

    Add tests

--- test/supertokens_test.dart
+++ test/supertokens_test.dart
@@ -1,28 +1,101 @@
+@Timeout(const Duration(seconds: 60))
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:supertokens/src/anti-csrf.dart';
 import 'package:supertokens/src/id-refresh-token.dart';
 import 'package:supertokens/supertokens.dart';
+import 'package:http/http.dart' as http;
+
+import 'test-utils.dart';
 
 void main() {
-  setUp(() async {
+  SuperTokensHttpClient networkClient =
+      SuperTokensHttpClient.getInstance(http.Client());
+
+  String loginAPIURL = "${SuperTokensTestUtils.testAPIBase}login";
+  String refreshTokenUrl = "${SuperTokensTestUtils.testAPIBase}refresh";
+  String userInfoAPIURL = "${SuperTokensTestUtils.testAPIBase}userInfo";
+  String logoutAPIURL = "${SuperTokensTestUtils.testAPIBase}logout";
+
+  int sessionExpiryCode = 401;
+
+  Future<void> startST({
+    int validity = 1,
+    double? refreshValidity,
+    bool disableAntiCSRF = false,
+  }) async {
+    await SuperTokensTestUtils.startSuperTokens(
+        validity: validity,
+        refreshValidity: refreshValidity,
+        disableAntiCSRF: disableAntiCSRF);
+  }
+
+  Future<void> _setUp() async {
     WidgetsFlutterBinding.ensureInitialized();
     SharedPreferences.setMockInitialValues({});
     SuperTokens.isInitCalled = false;
     await AntiCSRF.removeToken();
     await IdRefreshToken.removeToken();
+    await SuperTokensTestUtils.beforeEachTest();
+  }
+
+  setUp(() {
+    return _setUp();
   });
 
-  test("Test that requests fail when SuperTokens.initialise is not called", () {
-    // TODO: Add test case
-    expect(true, false);
+  tearDown(() async {
+    return SuperTokensTestUtils.afterEachTest();
   });
 
-  test("Test that mutiple calls to SuperTokens.initialise work as expected",
-      () {
-    // TODO: Add test case
-    expect(true, false);
+  test("Test that requests fail when SuperTokens.initialise is not called",
+      () async {
+    try {
+      await networkClient.post(Uri.parse(loginAPIURL));
+    } on http.ClientException catch (e) {
+      if (e.message !=
+          "SuperTokens.initialise must be called before using SuperTokensHttpClient") {
+        throw e;
+      }
+    } catch (e) {
+      throw e;
+    }
+  });
+
+  test("Test that multiple calls to SuperTokens.initialise work as expected",
+      () async {
+    await startST(validity: 5);
+
+    try {
+      SuperTokens.initialise(
+          refreshTokenEndpoint: refreshTokenUrl,
+          sessionExpiryStatusCode: sessionExpiryCode);
+
+      SuperTokens.initialise(
+          refreshTokenEndpoint: refreshTokenUrl,
+          sessionExpiryStatusCode: sessionExpiryCode);
+    } catch (e) {
+      fail("Calling initialise more than once failed");
+    }
+
+    http.Response response = await networkClient.post(Uri.parse(loginAPIURL));
+    if (response.statusCode != 200) {
+      fail("Login API failed");
+    }
+
+    try {
+      SuperTokens.initialise(
+          refreshTokenEndpoint: refreshTokenUrl,
+          sessionExpiryStatusCode: sessionExpiryCode);
+    } catch (e) {
+      fail("Calling initialise more than once failed");
+    }
+
+    http.Response userInfoResponse =
+        await networkClient.get(Uri.parse(userInfoAPIURL));
+    if (userInfoResponse.statusCode != 200) {
+      fail("UserInfo API failed");
+    }
   });
 
   test(
@@ -64,15 +137,72 @@ void main() {
 
   test(
       "Test that network requests without valid credentials throw session expired and do not trigger a call to the refresh endpoint",
-      () {
-    // TODO: Add test case
-    expect(true, false);
+      () async {
+    await startST(validity: 3);
+
+    try {
+      SuperTokens.initialise(
+          refreshTokenEndpoint: refreshTokenUrl,
+          sessionExpiryStatusCode: sessionExpiryCode);
+    } catch (e) {
+      throw e;
+    }
+
+    try {
+      http.Response response =
+          await networkClient.get(Uri.parse(userInfoAPIURL));
+
+      if (response.statusCode == 200) {
+        fail("userInfo API succeeded when it should have failed");
+      }
+
+      if (response.statusCode != sessionExpiryCode) {
+        fail("UserInfo status code did not match session expired");
+      }
+    } catch (e) {
+      throw e;
+    }
   });
 
   test("Test that the library works as expected when anti-csrf is disabled",
-      () {
-    // TODO: Add test case
-    expect(true, false);
+      () async {
+    await startST(validity: 3, refreshValidity: 2, disableAntiCSRF: true);
+
+    try {
+      SuperTokens.initialise(
+          refreshTokenEndpoint: refreshTokenUrl,
+          sessionExpiryStatusCode: sessionExpiryCode);
+    } catch (e) {
+      throw e;
+    }
+
+    http.Response response = await networkClient.post(Uri.parse(loginAPIURL));
+
+    if (response.statusCode != 200) {
+      fail("Login API failed");
+    }
+
+    await Future.delayed(Duration(seconds: 5));
+
+    http.Response userInfoResponse =
+        await networkClient.get(Uri.parse(userInfoAPIURL));
+
+    if (userInfoResponse.statusCode != 200) {
+      fail("userInfo API failed");
+    }
+
+    int refreshCounter = await SuperTokensTestUtils.getRefreshTokenCounter();
+    if (refreshCounter != 1) {
+      fail("Unexpected counter value: was $refreshCounter extected 1");
+    }
+
+    http.Response logoutResponse =
+        await networkClient.post(Uri.parse(logoutAPIURL));
+    if (logoutResponse.statusCode != 200) {
+      fail("Logout API failed");
+    }
+
+    expect(await SuperTokens.doesSessionExist(), false);
   });
 
   test(
---
+++ test/test-utils.dart
@@ -0,0 +1,60 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+class SuperTokensTestUtils {
+  static String testAPIBase = "http://127.0.0.1:8080/";
+  static http.Client nonInterceptedClient = http.Client();
+
+  static Future<void> beforeEachTest() async {
+    String beforeEachAPIURL = "${testAPIBase}beforeeach";
+    await nonInterceptedClient.post(Uri.parse(beforeEachAPIURL));
+  }
+
+  static afterEachTest() async {
+    String afterAPIURL = "${testAPIBase}after";
+    await nonInterceptedClient.post(Uri.parse(afterAPIURL));
+  }
+
+  static Future<void> startSuperTokens({
+    int validity = 1,
+    double? refreshValidity,
+    bool disableAntiCSRF = false,
+  }) async {
+    String startSTAPIURL = "${testAPIBase}startst";
+    String body = jsonEncode({
+      "accessTokenValidity": validity,
+    });
+
+    if (refreshValidity != null) {
+      body = jsonEncode({
+        "accessTokenValidity": validity,
+        "refreshTokenValidity": refreshValidity,
+        "disableAntiCSRF": disableAntiCSRF,
+      });
+    }
+
+    http.Response response = await nonInterceptedClient.post(
+      Uri.parse(startSTAPIURL),
+      headers: {"Content-Type": "application/json; charset=utf-8"},
+      body: body,
+    );
+
+    if (response.statusCode != 200) {
+      throw Exception("Error starting supertokens");
+    }
+  }
+
+  static Future<int> getRefreshTokenCounter() async {
+    String refreshCounterAPIURL = "${testAPIBase}refreshCounter";
+    http.Response response =
+        await nonInterceptedClient.get(Uri.parse(refreshCounterAPIURL));
+
+    if (response.statusCode != 200) {
+      throw Exception("Error getting refresh token counter");
+    }
+
+    Map<String, dynamic> responseBody = jsonDecode(response.body);
+    return responseBody["counter"];
+  }
+}
