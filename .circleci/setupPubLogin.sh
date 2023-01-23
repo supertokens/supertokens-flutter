f994ae4c727429821ee8bd91d41ca8f557c988c2
rishabhpoddar <rishabh.poddar@gmail.com>
2023-01-23 18:44:41 +0530

    removes publish job

--- .circleci/setupPubLogin.sh
+++
@@ -1,8 +0,0 @@
-cat <<EOF > ~/.pub-cache/credentials.json
-{
-  "accessToken":"$PUB_DEV_PUBLISH_ACCESS_TOKEN",
-  "refreshToken":"$PUB_DEV_PUBLISH_REFRESH_TOKEN",
-  "tokenEndpoint":"$PUB_DEV_PUBLISH_TOKEN_ENDPOINT",
-  "scopes":["https://www.googleapis.com/auth/userinfo.email","openid"],
-  "expiration":$PUB_DEV_PUBLISH_EXPIRATION
-}
