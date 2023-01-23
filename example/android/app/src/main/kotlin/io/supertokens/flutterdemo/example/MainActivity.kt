8d1ff7616eb09c6625947c60217a25d5717f701f
rishabhpoddar <rishabh.poddar@gmail.com>
2023-01-23 16:30:37 +0530

    cleans up the repo further

--- example/android/app/src/main/kotlin/io/supertokens/flutterdemo/example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package io.supertokens.flutterdemo.example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/src/constants.dart
+++ lib/src/constants.dart
@@ -2,7 +2,6 @@ const String antiCSRFHeaderKey = "anti-csrf";
 const String superTokensPlatformHeaderKey = "supertokens-sdk-name";
 const String superTokensSDKVersionHeaderKey = "supertokens-sdk-version";
 const String superTokensPlatformName = "flutter";
-// TODO: automate updating this variable whenever the package verison changes (similar to supertokens-ios)
 const String idRefreshHeaderKey = "id-refresh-token";
 
 const String frontTokenHeaderKey = "front-token";
