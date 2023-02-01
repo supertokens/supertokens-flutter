317a41d8c94a4f16a7d948a59ffdcdf77ddf7993
Nemi Shah <nemishah1212@gmail.com>
2023-02-01 18:25:17 +0530

    Update FDI, CHANGELOG and package version

--- lib/src/version.dart
+++ lib/src/version.dart
@@ -1,13 +1,6 @@
 class Version {
   static List<String> supported_fdi = [
-    "1.8",
-    "1.9",
-    "1.10",
-    "1.11",
-    "1.12",
-    "1.13",
-    "1.14",
-    "1.15"
+    "1.16"
   ];
-  static String sdkVersion = "0.0.1";
+  static String sdkVersion = "0.1.0";
 }
