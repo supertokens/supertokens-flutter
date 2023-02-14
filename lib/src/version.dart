9681a13325276532a0419029371b0c2a263f59eb
Iresh Sharma <iresh.sharma8@gmail.com>
2023-02-14 12:50:43 +0530

    version updated in version.dart

--- lib/src/version.dart
+++ lib/src/version.dart
@@ -1,6 +1,4 @@
 class Version {
-  static List<String> supported_fdi = [
-    "1.16"
-  ];
-  static String sdkVersion = "0.1.1";
+  static List<String> supported_fdi = ["1.16"];
+  static String sdkVersion = "0.1.2";
 }
