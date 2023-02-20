e8be50075d7c4f7272129d26576184bbf0ad6e38
Iresh Sharma <iresh.sharma8@gmail.com>
2023-02-20 14:04:05 +0530

    config.yml updates to add workflow

--- lib/http.dart
+++ lib/http.dart
@@ -1,9 +1,3 @@
-/// Flutter implementation of SuperTokens session management.
-/// This library is a pure dart implementation and does not use platform channels.
-/// Use [Client] to use SuperTokens with the [http](https://pub.dev/packages/http) package.
-/// [Client] uses SharedPreferences to store cookies across app launches
-// library supertokens;
-
 export "src/supertokens-http-client.dart"
     show Client, get, post, patch, delete, put, send
     hide UnauthorisedResponse, UnauthorisedStatus;
