460c0e4c4784dddfe14b57692ca34cfb25eeac99
Nemi Shah <nemishah1212@gmail.com>
2023-05-08 16:30:32 +0530

    Remove tsc compile from test cicd config

--- .circleci/setupAndTestWithFreeCore.sh
+++ .circleci/setupAndTestWithFreeCore.sh
@@ -47,7 +47,6 @@ echo $SUPERTOKENS_API_KEY > apiPassword
 cd ../project/testHelpers/server/
 npm i -d --force
 npm i git+https://github.com:supertokens/supertokens-node.git#$2
-npx tsc -p tsconfig.json
 TEST_MODE=testing INSTALL_PATH=../../../supertokens-root node . &
 pid=$!
 cd ../../
