32ea22d0e378774cc76ee281b95dfc38647a72fb
daniil-borovoy <daniilborovoy42@gmail.com>
2023-11-12 22:50:14 +0300

    docs: bump sdk version to 0.2.9

--- lib/dio.dart
+++ lib/dio.dart
@@ -1,2 +1,2 @@
 export 'src/dio-interceptor-wrapper.dart' show SuperTokensInterceptorWrapper;
-export "src/supertokens_dio_extension.dart" show SuperTokensDioExtension;
+export 'src/supertokens_dio_extension.dart' show SuperTokensDioExtension;
