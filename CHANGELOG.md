a46a3a09be3158b77a7835bae07d1cbd623855ed
rishabhpoddar <rishabh.poddar@gmail.com>
2024-03-14 15:46:03 +0530

    new version for fdi 1.19 support

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,9 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [0.3.1] - 2024-03-14
+- New FDI version support: 1.19
+
 ## [0.3.0] - 2023-11-13
 
 - Added Dio extension for interceptor setup
--- lib/src/version.dart
+++ lib/src/version.dart
@@ -1,4 +1,4 @@
 class Version {
-  static List<String> supported_fdi = ["1.16", "1.17", "1.18"];
-  static String sdkVersion = "0.3.0";
+  static List<String> supported_fdi = ["1.16", "1.17", "1.18", "1.19"];
+  static String sdkVersion = "0.3.1";
 }
