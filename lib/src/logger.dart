b9398618539d843563a8df4f23150a70f690b4bb
Deepjyoti Barman <deepjyoti@supertokens.com>
2024-09-26 08:55:52 +0530

    Add init logger definition for debug logging

---
+++ lib/src/logger.dart
@@ -0,0 +1,29 @@
+import 'version.dart';
+
+const String supertokensDebugNamespace = 'com.supertokens';
+
+bool _supertokensWebsiteLogging = false;
+
+// Enable debug logging
+void enableLogging() {
+  _supertokensWebsiteLogging = true;
+}
+
+// Disable debug logging
+void disableLogging() {
+  _supertokensWebsiteLogging = false;
+}
+
+// Logs a debug message
+//
+// This function will only log the debug message if debug logging
+// is enabled.
+//
+// It can be enabled/disabled through `enableLogging` & `disableLogging`
+// functions
+void logDebugMessage(String message) {
+  if (_supertokensWebsiteLogging) {
+    print(
+        '$supertokensDebugNamespace {t: "${DateTime.now().toIso8601String()}", message: "$message", supertokens-react-native: "${Version.sdkVersion}"}');
+  }
+}
